ab7c9c8aa5b9d76e4a182bf67d47deba3976eb94
Yann Darwish <yann.darwish@gmail.com>
2022-06-03 15:40:35 +0200

    modal close function done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,9 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// my elements
+const closeModalBtn = document.querySelector(".close");
+const closeBtn = document.querySelector('.close-btn');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +21,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal event
+closeModalBtn.addEventListener("click", closeModal);
+closeBtn.addEventListener('click', closeModal)
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
